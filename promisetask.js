4d555d7f848950e9af06930d31fea772a646005d
purush <purushothaman@ngpwebsmart.com>
2022-08-24 18:52:29 +0530

    f

--- promisetask.js
+++ promisetask.js
@@ -3,24 +3,21 @@ function random(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random()*(max - min+1))+min;
 }
-const persons=[1,2,3,4,5];
-
-    for (let i=0;i<persons.length;i++){
-      let  inf =  new Promise((resolve,reject)=>{
-        let ran=random(1,10);
-        setTimeout(()=>{
-          console.log("person "+persons[i]+ " was going in:");
-          resolve("The time consumed was " +ran+" seconds");
-          reject("nothing");
-        },ran*1000)
+let prompt = require('prompt-sync')();
+let persons= prompt('Enter the total no of patients: ');
+for(let i=1;i<=persons;i++){
+        let  inf =  new Promise((resolve,reject)=>{
+            let ran=random(1,10);
+            console.log("person "+i+ " was going in.");
+            setTimeout(()=>{
+                resolve("The time consumed was " +ran+" seconds");
+            },ran*1000);
         });
         async function myfunction(){
-          let wait=await  inf;
-          console.log(wait);
+            let wait=await  inf;
+            console.log(wait);
         }
-        myfunction();
-
-    }
-  
+      await  myfunction();
+}
     
     
