3251a4f80944176f629a2f34c353ee2e88af7051
purush <purushothaman@ngpwebsmart.com>
2022-08-25 11:28:17 +0530

    f

---
+++ mathcal.js
@@ -0,0 +1,16 @@
+function add(a, b) {
+    return typeof (a + b);
+}
+console.log(add(5, 6));
+function sub(a, b) {
+    return typeof (a - b);
+}
+console.log(sub(5, 6));
+function mul(a, b) {
+    return typeof (a * b);
+}
+console.log(mul(5, 6));
+function div(a, b) {
+    return typeof (a / b);
+}
+console.log(div(5, 6));
---
+++ mathcal.ts
@@ -0,0 +1,19 @@
+function add(a:number,b:number){
+    return typeof(a+b);
+}
+console.log(add(5,6));
+
+function sub(a:number,b:number){
+    return typeof(a-b);
+}
+console.log(sub(5,6));
+
+function mul(a:number,b:number){
+    return typeof(a*b);
+}
+console.log(mul(5,6));
+
+function div(a:number,b:number){
+    return typeof(a/b);
+}
+console.log(div(5,6));
