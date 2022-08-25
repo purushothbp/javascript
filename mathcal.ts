9665f4cd3c68c34807dcf821ef092e25cb435f5a
purushothbp <72174403+purushothbp@users.noreply.github.com>
2022-08-25 14:07:05 +0530

    Update mathcal.ts

--- mathcal.ts
+++ mathcal.ts
@@ -1,23 +1,23 @@
 function add(a:number,b:number){
     console.log(a+b);
-    return typeof(a+b);
+
 }
 console.log(add(5,6));
 
 function sub(a:number,b:number){
     console.log(a-b);
-    return typeof(a-b);
+
 }
 console.log(sub(5,6));
 
 function mul(a:number,b:number){
     console.log(a*b);
-    return typeof(a*b);
+
 }
 console.log(mul(5,6));
 
 function div(a:number,b:number){
     console.log(a/b);
-    return typeof(a/b);
+
 }
 console.log(div(12,6));
